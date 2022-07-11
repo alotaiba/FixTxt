6acd5def25cf03c4daa633e64b1401066954aa07
Abdulrahman Saleh Khamis <abdulrahman@mawqey.com>
2022-07-11 23:20:02 +0300

    added more content on the homepage

--- pages/index.js
+++ pages/index.js
@@ -6,6 +6,10 @@ import Container from "@mui/material/Container";
 import Box from "@mui/material/Box";
 import IconButton from "@mui/material/IconButton";
 import Stack from "@mui/material/Stack";
+import Image from "next/image";
+import instagramUseCaseImage from "../public/images/FixTxt_Instagram.png";
+import notionUseCaseImage from "../public/images/FixTxt_Notion.png";
+
 import {
   FormatTextdirectionLToR as FormatTextdirectionLToRIcon,
   FormatTextdirectionRToL as FormatTextdirectionRToLIcon,
@@ -65,7 +69,7 @@ export default function Home() {
 
   return (
     <Container
-      maxWidth="sm"
+      maxWidth="md"
       sx={{
         display: "flex",
         flexDirection: "column",
@@ -131,10 +135,44 @@ export default function Home() {
             style={{ width: "100%" }}
           />
         </Box>
+        <Box sx={{ mt: 5 }}>
+          <Typography variant="h4">What is FixTxt?</Typography>
+          <Typography variant="body1" gutterBottom>
+            It is a web tool that helps you write text in proper flow of the
+            language. Some web services and apps do not support RTL languages
+            such as Arabic, and when you try to mix LTR characters within the
+            text, the whole flow gets corrupted, to the point that sometimes it
+            is not understandable.
+          </Typography>
+        </Box>
+        <Box sx={{ mt: 5 }}>
+          <Typography variant="h4">How to use FixTxt?</Typography>
+          <Typography variant="body1" gutterBottom>
+            <ol>
+              <li>Copy the text that you want to fix in the text area.</li>
+              <li>Click on the change text direction icon.</li>
+              <li>Copy the the result by clicking on the copy icon.</li>
+            </ol>
+          </Typography>
+        </Box>
+        <Box sx={{ mt: 5 }}>
+          <Typography variant="h4">Use Cases</Typography>
+          <Typography variant="body1" gutterBottom>
+            You can use the text to fix your RTL &lt;-&gt; LTR mixed text in a
+            lot of placed around the web.
+          </Typography>
+          <Typography variant="h5">Instagram</Typography>
+          <Image src={instagramUseCaseImage} alt="Picture of the author" />
+          <Typography variant="h5">Notion</Typography>
+          <Image src={notionUseCaseImage} alt="Picture of the author" />
+          <Typography variant="body1" gutterBottom>
+            Plus much much more...
+          </Typography>
+        </Box>
       </Container>
 
       <Container component="footer" sx={{ mt: "auto", py: 3 }}>
-        Made with <span>♥</span> by{" "}
+        Made with <span>♥</span> in Qatar by{" "}
         <a href="https://mawqey.com/">Abdulrahman Saleh Khamis</a>
       </Container>
     </Container>
